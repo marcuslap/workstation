934b235662a74196a81016206b2d0f2e7cba3da6
Marcus LaPilusa <glapper@gmail.com>
2018-05-06 23:22:03 +0000

    first commit

---
+++ recipes/setup.rb
@@ -0,0 +1,20 @@
+package 'tree' do
+  action :install
+end
+package 'ntp' do
+  action :install
+end
+
+package 'vim-enhanced'
+
+package 'nano'
+
+package 'git' do 
+  action :install
+end
+
+file '/etc/motd' do
+  content 'Property of ...'
+  owner 'root'
+  group 'root'
+end
